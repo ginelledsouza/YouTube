c28f7ad88fee62c01516fb852e0bcd786385e068
Ginelle D'souza <64312327+ginelledsouza@users.noreply.github.com>
2022-05-15 22:56:10 +0530

    YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -2,24 +2,35 @@ import json
 import pandas as pd
 import requests
 
-channel_id = <channel_id>
 key = <yt_data_api>
 
-url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={key}'
+ytdata = pd.DataFrame()
+ytchannel = ["sonymusicindiaSME","sonypalindia","marvel"]
 
-json_url = requests.get(url)
-data = json.loads(json_url.text)
+def retrive_data(url):
 
-try:
+    json_url = requests.get(url)
+    value = json.loads(json_url.text)
+    
+    return value
+
+for i in ytchannel:
+    username = i
+    
+    url = f'https://www.googleapis.com/youtube/v3/channels?key={key}&forUsername={username}&part=id'
+    data = retrive_data(url)
+    
+    channel_id = data['items'][0]['id']
+    url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={key}'
+    data = retrive_data(url)
+    
     details = data['items'][0]['statistics']
-except KeyError:
-    print('Could not get channel statistics')
-    details = {}
-
-url = f"https://www.googleapis.com/youtube/v3/search?key={key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=20"
-
-json_url = requests.get(url)
-data = json.loads(json_url.text)
-
-details['channelTitle'] = data['items'][0]['snippet']['channelTitle']
-channel_statistics = pd.DataFrame([details])
+    
+    url = f"https://www.googleapis.com/youtube/v3/search?key={key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=20"
+    
+    data = retrive_data(url)
+    
+    details['channelTitle'] = data['items'][0]['snippet']['channelTitle']
+    channel_statistics = pd.DataFrame([details])
+    
+    ytdata = ytdata.append(channel_statistics)
